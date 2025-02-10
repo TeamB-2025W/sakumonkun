27f68f36cf2c8ccdbbe52f5b998e7523980ef269
イズミ <izmk83@google.com>
2025-02-11 01:11:24 +0900

    [fix] admin.py 全てのテーブルを編集可能に修正

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User, Test, Question, QuestionChoice, Session, Answer
 
-# Register your models here.
+
+admin.site.register(User)
+admin.site.register(Test)
+admin.site.register(Question)
+admin.site.register(QuestionChoice)
+admin.site.register(Session)
+admin.site.register(Answer)
