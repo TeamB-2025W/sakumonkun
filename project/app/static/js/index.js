cfce15f73c29e1d455d3ad957c8a8867e8dcc092
ゆういち <yuichi-star-alchemist@users.noreply.github.com>
2025-02-18 22:04:44 +0900

    [add] modalにテストタイトルを表示するように

--- project/app/static/js/index.js
+++ project/app/static/js/index.js
@@ -1,10 +1,14 @@
 "use strict";
 
-function toggleModal() {
+function toggleModal(testTitle) {
   // mainタグのhidden切り替え
   const homeMain = document.getElementsByClassName("home-main")[0];
   homeMain.classList.toggle("hidden");
   // modalのhidden切り替え
   const modal = document.getElementsByClassName("modal")[0];
   modal.classList.toggle("hidden");
+
+  if (testTitle) {
+    document.getElementById("test-title-viewer").textContent = testTitle;
+  }
 }
