29b19b06c1e013d5cc91d65b99bbd714c09da974
イズミ <izmk83@google.com>
2025-02-24 11:57:41 +0900

    [add, fix] Argon2idをインストール migrationを修正 シェルスクリプトで動くか試してみたが DBへのパスワード認証でエラーとなるため断念

---
+++ init_datas/initial_data_migrate.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# MySQLでDBを削除・作成（警告を回避）
+# DBにScriptで接続できない設定があるっぽい
+
+# echo "Dropping and recreating database..."
+# MYSQL_PWD=root docker-compose exec -T db mysql -u root -e "DROP DATABASE sakumon_db;"
+# MYSQL_PWD=root docker-compose exec -T db mysql -u root -e "CREATE DATABASE sakumon_db;"
+
+# MySQLの準備ができるまで待機
+# sleep 3
+
+# マイグレーションファイルの削除
+echo "Removing migration files..."
+rm -rf ../project/app/migrations/*
+mkdir -p ../project/app/migrations
+touch ../project/app/migrations/__init__.py  # migrationsディレクトリをDjangoが認識できるようにする
+
+# Djangoマイグレーション
+echo "Applying migrations..."
+docker-compose exec web python manage.py makemigrations app
+
+# 既存のデータ移動
+echo "Moving initial data migration files..."
+cp 0002_create_superuser.py ../project/app/migrations/0002_create_superuser.py
+cp 0003_initial_data.py ../project/app/migrations/0003_initial_data.py
+
+# Djangoの準備ができるまで少し待つ
+sleep 2
+
+docker-compose exec web python manage.py migrate
+
+# マイグレーションの適用状況を表示
+echo "Current migrations for app:"
+docker-compose exec web python manage.py showmigrations app | tee /dev/tty
+
+echo "Database reset and migration complete!"
