d2b5625b2f9999a555a87a7b74cc373100132804
EC2 Default User <ec2-user@ip-10-0-3-5.ap-northeast-1.compute.internal>
2025-02-24 03:58:24 +0000

    ”インフラ”

--- project/project/settings.py
+++ project/project/settings.py
@@ -25,11 +25,7 @@ SECRET_KEY = 'django-insecure-8icgv5+zx!p=eqopt=)3oz$r)84c5(r=7!(rr=v=gm)7um^ei6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    '0.0.0.0',
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
